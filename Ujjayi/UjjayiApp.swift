4ef28fcfa958cc005ed758db5ef2fb3d8d241417
allocinit <instancetype@gmail.com>
2022-02-04 11:13:53 +0300

    Initial Commit

---
+++ Ujjayi/UjjayiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UjjayiApp.swift
+//  Ujjayi
+//
+//  Created by Aleksandr Borisov on 04.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct UjjayiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
